bfbbed4b4a7251eec29b1bfd632416130d07b67a
Baptiste Jamin <baptiste@crisp.chat>
2025-09-10 11:41:57 +0200

    Use patched engine io

--- dist/socket.io.js
+++ dist/socket.io.js
@@ -761,9 +761,7 @@
   };
 
   var globalThis = (function () {
-    if (typeof self !== "undefined") {
-      return self;
-    } else if (typeof window !== "undefined") {
+    if (typeof window !== "undefined") {
       return window;
     } else {
       return Function("return this")();
